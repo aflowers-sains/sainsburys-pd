faa6eb25b4207121aec6ea12115406f60b3050c0
Andy Flowers <andrew.flowers@sainsburys.co.uk>
2021-04-30 16:06:49 +0100

    simple extension functions

--- KotlinCLI/src/main/kotlin/UnusedParams.kt
+++ KotlinCLI/src/main/kotlin/UnusedParams.kt
@@ -2,8 +2,13 @@ class UnusedParams {
     val stuff: List<MyData> = listOf(MyData("data1", 1))
 
     fun doStuff() {
-        stuff.stream().mapToDouble { (_, data2) -> data2.toDouble() }.forEach { dbl: Double -> println(dbl) }
+        stuff.stream().mapToDouble { (_, data2) -> data2.toDouble() }
+            .forEach { dbl: Double -> println(dbl.toMoneyString()) }
     }
+
+    private fun Double.toMoneyString() = this.formattedAs("%#.2f")
+
+    private fun Double.formattedAs(format: String) = String.format(format, this)
 }
 
 fun main() {
