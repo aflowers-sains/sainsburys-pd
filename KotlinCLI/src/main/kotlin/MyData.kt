3f30f1f21533e67b96eba33ce93947da74b9df91
Andy Flowers <andrew.flowers@sainsburys.co.uk>
2021-04-30 15:30:35 +0100

    get on a property

--- KotlinCLI/src/main/kotlin/MyData.kt
+++ KotlinCLI/src/main/kotlin/MyData.kt
@@ -1,5 +1,15 @@
 data class MyData(val data1: String, val data2: Int) {
+
+    val combined: String get() = "Data::$data1 && Data::$data2"
+
     override fun toString(): String {
         return "MyData(data1='$data1', data2=$data2)"
     }
+}
+
+
+fun main() {
+    val md = MyData("d1", 1)
+
+    println("combined = ${md.combined}")
 }
